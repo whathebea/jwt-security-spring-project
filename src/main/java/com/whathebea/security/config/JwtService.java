7c776fdc1e41e92f4831b7de99b317648598d596
whathebea <bea.graciast@gmail.com>
2023-01-12 18:20:08 -0300

    created config package for jwt configs, created filter and service still in progress

---
+++ src/main/java/com/whathebea/security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.whathebea.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    // TODO: get claims and compare the username with the one in the database
+    public String extractUsername(String token) {
+        return null;
+    }
+}
