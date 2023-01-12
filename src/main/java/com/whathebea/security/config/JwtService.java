4044a71870c67d9095a25e39cf01aae203263330
whathebea <bea.graciast@gmail.com>
2023-01-12 18:40:43 -0300

    refactored extractusername method in jwtservice

--- src/main/java/com/whathebea/security/config/JwtService.java
+++ src/main/java/com/whathebea/security/config/JwtService.java
@@ -4,11 +4,18 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "452948404D635166546A576E5A7234753778214125442A472D4A614E64526755";
     public String extractUsername(String token) {
-        return extractAllClaims(token).getSubject();
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
