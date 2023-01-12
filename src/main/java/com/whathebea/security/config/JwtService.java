11166a3cd968d496910c53759ccfd4dc2a818837
whathebea <bea.graciast@gmail.com>
2023-01-12 18:38:41 -0300

    implemented jwt methods in jwtservice

--- src/main/java/com/whathebea/security/config/JwtService.java
+++ src/main/java/com/whathebea/security/config/JwtService.java
@@ -1,11 +1,27 @@
 package com.whathebea.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    // TODO: get claims and compare the username with the one in the database
+    private static final String SECRET_KEY = "452948404D635166546A576E5A7234753778214125442A472D4A614E64526755";
     public String extractUsername(String token) {
-        return null;
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private byte[] getSecretKey() {
+        byte[] decodedKey = java.util.Base64.getDecoder().decode(SECRET_KEY);
+        return decodedKey;
     }
 }
