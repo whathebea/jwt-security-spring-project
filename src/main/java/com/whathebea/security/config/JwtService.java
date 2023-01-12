0a1a1282756e3b03d74e14e2083326272fc53d6a
whathebea <bea.graciast@gmail.com>
2023-01-12 18:52:09 -0300

    added jwt validation methods to jwtservice

--- src/main/java/com/whathebea/security/config/JwtService.java
+++ src/main/java/com/whathebea/security/config/JwtService.java
@@ -2,13 +2,19 @@ package com.whathebea.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "452948404D635166546A576E5A7234753778214125442A472D4A614E64526755";
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -19,16 +25,28 @@ public class JwtService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSecretKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        return Jwts.parserBuilder().setSigningKey(getSecretKey()).build().parseClaimsJws(token).getBody();
     }
 
-    private byte[] getSecretKey() {
-        byte[] decodedKey = java.util.Base64.getDecoder().decode(SECRET_KEY);
-        return decodedKey;
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new java.util.Date(System.currentTimeMillis())).setExpiration(new java.util.Date(System.currentTimeMillis() + 9000)).signWith(getSecretKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public java.util.Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new java.util.Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Key getSecretKey() {
+        byte[] keyBytes = java.util.Base64.getDecoder().decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
