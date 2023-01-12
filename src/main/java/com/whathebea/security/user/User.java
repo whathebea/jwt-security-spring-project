c057a13e04a035956df9c40a087c88958696946c
whathebea <bea.graciast@gmail.com>
2023-01-12 17:53:23 -0300

    added lombok and created user entity

---
+++ src/main/java/com/whathebea/security/user/User.java
@@ -0,0 +1,32 @@
+package com.whathebea.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer ID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+}
